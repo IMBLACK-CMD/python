5f082ecfd62a26370275effde75534083a9eed6d
IMBLACK-CMD <imblackcmd@gmail.com>
2020-08-25 12:45:47 +0300

    python function and return statement file

--- functions/pythoFunction.py
+++ functions/pythoFunction.py
@@ -1,4 +1,11 @@
 def function_name(parameters):
 	""" python comment """
-	""" function body goes""
-	
+	""" function body goes"""
+
+
+
+def greetings(name):
+	"""function to pass greetings"""
+	print("Hello " +name+  " How do you do")
+
+greetings('Nelson')
---
+++ functions/returnstatement.py
@@ -0,0 +1,8 @@
+def absolute_value(num):
+    """ This function returns an absolute value"""
+    if num >= 0:
+        return num
+    else:
+        return -num
+print(absolute_value(10))
+print(absolute_value(-5))
